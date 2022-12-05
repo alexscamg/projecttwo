19517f6745896f401e911ad12acb7d6b226411ab
Luc <102383668+lucianoalvarez22@users.noreply.github.com>
2022-12-05 16:54:02 +0100

    commitactualizacion
    actualizacion luciano

--- ale_luciano.py
+++ ale_luciano.py
@@ -8,3 +8,36 @@ def suma(a, b):
 
 suma(a,b)
 multiplica(val1, val2)
+
+def multiplica(val1, val2):
+  return val1 * val2
+
+
+def suma(a, b):
+  return a + b
+
+
+suma(a,b)
+multiplica(val1, val2)
+
+def multiplica(val1, val2):
+  return val1 * val2
+
+
+def suma(a, b):
+  return a + b
+
+
+suma(a,b)
+multiplica(val1, val2)
+
+def multiplica(val1, val2):
+  return val1 * val2
+
+
+def suma(a, b):
+  return a + b
+
+
+suma(a,b)
+multiplica(val1, val2)
