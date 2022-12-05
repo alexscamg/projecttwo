0202d3443589794bf296a2ef2001eb736d6955d2
Luc <102383668+lucianoalvarez22@users.noreply.github.com>
2022-12-05 17:08:47 +0100

    actualiluciano
    actual comit 2

--- ale_luciano.py
+++ ale_luciano.py
@@ -24,20 +24,4 @@ def multiplica(val1, val2):
   return val1 * val2
 
 
-def suma(a, b):
-  return a + b
-
-
-suma(a,b)
-multiplica(val1, val2)
-
-def multiplica(val1, val2):
-  return val1 * val2
-
-
-def suma(a, b):
-  return a + b
-
-
-suma(a,b)
-multiplica(val1, val2)
+MODIFICACION LUCOIANOOO
